ff5a72bab9d04d2e1ccf6fcd81237ec882db410f
unknown <codewithsrobins1@gmail.com>
2021-05-15 20:00:40 -0700

    initial files

---
+++ notes.js
@@ -0,0 +1,8 @@
+//NextJS = React Framework for Production
+
+//Pros and Cons List - https://pagepro.co/blog/pros-and-cons-of-nextjs/
+
+//To run development server -- npm run dev
+
+//Utilizing Theme UI for styling
+    //use sx={styles...}
